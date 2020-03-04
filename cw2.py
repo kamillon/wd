1965ed6c4e2ae2a33c583730dc2c67bdbd15d64a
kamillon <kamil464646@wp.pl>
2020-03-04 17:31:59 +0100

    commit

---
+++ cw2.py
@@ -0,0 +1,65 @@
+# wejscie = input()
+# print(wejscie)
+# print(type(wejscie))
+# import sys
+# wejscie2 = sys.stdin.readline()
+# print(wejscie2)
+# print(type(wejscie2))
+
+# dfsdfsdf
+# dfsdfsdf
+# <class 'str'>
+# dfsdfsdf
+# dfsdfsdf
+
+# <class 'str'>
+
+liczba = 10
+if liczba < 10:
+  print("costam")
+elif liczba > 0:
+  print("costam2")
+else:
+  print("klapa")
+
+# and or not
+# != różny od
+if liczba > 0 and liczba <11:
+  print("OK")
+
+#krócej
+if 0 < liczba < 11:
+  print("OK")
+
+
+  # foreach
+imie = "Ariadna"
+for litera in imie:
+  print(litera, end='')
+print()
+
+# range to generator
+range(5) # start, stop, step
+# stop -> stop - step
+print(range(5))
+print(type(range(5)))
+print(list(range(2, 10, 2)))
+print(list(range(10, 2, -2)))
+print(list(range(0, -10, -2)))
+
+# [od:do]
+print(imie[2])
+print(imie[2:3])
+print(imie[::2])
+
+for liczba in range(5):
+  pass
+for litera int imie[::2]:
+  pass
+
+def funkcja():
+  pass
+
+
+
+
