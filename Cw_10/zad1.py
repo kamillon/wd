2b3b8bcf10961d4792666649d5e0900f1c3d254f
kamillon <kamil464646@wp.pl>
2020-05-12 22:44:06 +0200

    matplotlib_1

---
+++ Cw_10/zad1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = np.arange(1,21)
+plt.plot(x, 1/x, label='f(x)=1/x')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.axis([0,len(x),0,1])
+plt.show()
